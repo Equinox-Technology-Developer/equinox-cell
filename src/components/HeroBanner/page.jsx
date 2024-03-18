1dfbb8ffee8327cb2c74ae1e0fdcecbdeac2a3f1
yuhariermy <yuhariermy@gmail.com>
2024-03-18 12:18:31 +0700

    Components | Hero Banner

--- src/components/HeroBanner/page.jsx
+++ src/components/HeroBanner/page.jsx
@@ -1,29 +1,28 @@
-import styles from "./HeroBanner.module.scss"
-
-const HeroBanner = ({ title, subtitle, imageUrl, highlightedWord }) => {
-  const parts = title.split(" ");
-  let highlightedIndexes = [];
-
-  for (let i = 0; i < parts.length; i++) {
-    if (parts[i].toLowerCase() === highlightedWord.toLowerCase()) {
-      highlightedIndexes.push(i);
-    }
-  }
-
+const HeroBanner = ({ title, subtitle, imageUrl }) => {
+  const backgroundStyle = {
+    backgroundImage: `url(${imageUrl})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '700px',
+  };
 
   return (
-    <div className={styles.hero_banner} style={{ backgroundImage: `url(${imageUrl})` }}>
-      <div className={styles.hero_content}>
-        <h1>
-          {parts.map((part, index) => (
-            <span key={index} className={highlightedIndexes.includes(index) ? 'highlight': ""}>
-              {part}{' '}
-            </span>
-          ))}
-        </h1>
-        <p>{subtitle}</p>
+    <section className="h-full w-full">
+      <div style={backgroundStyle}>
+        <div className="absolute inset-0 mt-[76px] h-[700px] w-full bg-gradient-to-b from-[#66666600] to-[#0000009E]">
+          <div className="absolute inset-0 flex h-[676px] w-full items-center justify-center text-center">
+            <div className="mx-auto max-w-[1086px] space-y-8">
+              <h1 className="text-[40px] font-bold leading-[48px] text-white">
+                {title}
+              </h1>
+              <p className="font-regular text-[20px] leading-[27.9px] text-white">
+                {subtitle}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
