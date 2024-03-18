ab49636a560e1062626bc1071662da45331a3b53
yuhariermy <yuhariermy@gmail.com>
2024-03-18 12:19:52 +0700

    Components | Export CapitalizeHeading, HeroBanner, JobCategories, SearchJobs

---
+++ src/components/page.jsx
@@ -0,0 +1,6 @@
+import CapitalizeHeading from './CapitalHeading/page';
+import HeroBanner from './HeroBanner/page';
+import SearchJobs from './SearchJobs/pages';
+import JobCategories from './JobCategories/page';
+
+export { CapitalizeHeading, HeroBanner, SearchJobs, JobCategories };
