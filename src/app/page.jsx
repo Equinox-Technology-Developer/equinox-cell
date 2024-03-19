73224859c0f0b1f22f956112ce29654d5365da53
muhreynaldii <reynaldi180101@gmail.com>
2024-03-19 11:38:20 +0700

    add button subs and fix banner homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -32,20 +32,19 @@ export default function Home() {
           alt="hero-image"
           className="h-[676px] w-full object-cover object-center"
         />
-        <div className="absolute inset-0 mt-[76px] h-[676px] w-full bg-gradient-to-b from-[#66666600] to-[#0000009E]">
-          <div className="absolute inset-0 flex h-[676px] w-full items-center justify-center text-center">
-            <div className="mx-auto max-w-[1086px] space-y-8">
-              <h1 className="text-[40px] font-bold leading-[48px] text-white">
-                Accelerate Your Career with Us:{' '}
-                <span className="text-[#59D5E0]">Fast Track </span>,{' '}
-                <span className="text-[#59D5E0]">Big Bonuses </span>, and
-                <span className="text-[#59D5E0]"> Elite Benefits </span> Await
-                Exceptional Talent!
-              </h1>
-              <p className="font-regular text-[20px] leading-[27.9px] text-white">
-                Our talent assessments screen and identify the best candidates
-                and make your hiring decisions faster, easier, and bias-free.
-              </p>
+        <div className="absolute inset-0 mt-[80px] h-[676px] w-full bg-gradient-to-l from-[#66666600] to-[#0000009E]">
+          <div className="absolute inset-0 flex h-[676px] w-full items-center justify-start text-start">
+            <div className="container mx-auto w-full space-y-8">
+              <div className="w-1/2 space-y-[24px]">
+                <h1 className="text-[40px] font-bold leading-[48px] text-white">
+                  Innovating Goods for The <br />
+                  <span className="text-secondary-500">Greater Good</span>
+                </h1>
+                <p className="font-regular text-[20px] leading-[27.9px] text-white">
+                  Our talent assessments screen and identify the best candidates
+                  and make your hiring decisions faster, easier, and bias-free.
+                </p>
+              </div>
             </div>
           </div>
         </div>
