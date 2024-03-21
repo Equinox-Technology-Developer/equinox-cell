c0ad762e72a847558e859eabf1c0f83b6a701340
ermyyuhari <yuhariermy@gmail.com>
2024-03-21 13:57:15 +0700

    Job - Submit Form | Templatting

--- src/app/career/[slug]/[submit-form]/page.jsx
+++ src/app/career/[slug]/[submit-form]/page.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import Layout from '@/components/Layout/page';
+import { getJobsBySlug } from '@/lib/jobs/page';
+import Link from 'next/link';
 
-const page = () => {
-  return <div>page</div>;
+const JobDetailsPage = ({ params: { slug } }) => {
+  const job = getJobsBySlug(slug);
+  return (
+    <>
+      <Layout>
+        {/* Banner */}
+        {/* Banner Job Detail */}
+        <section className="w-full bg-gradient-to-r from-secondary-500 to-primary-500">
+          <div className="container mx-auto py-20 ">
+            <h1 className="py-8 text-center text-4xl font-bold font-bold text-white">
+              {job.name}
+            </h1>
+          </div>
+        </section>
+
+        {/* Input Field */}
+        <section>
+          <div className="container mx-auto py-16">
+            
+          </div>
+        </section>
+      </Layout>
+    </>
+  );
 };
 
-export default page;
+export default JobDetailsPage;
