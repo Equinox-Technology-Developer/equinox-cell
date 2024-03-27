98b6e9b3f21c5adec073828a74ae04327d1be17a
muhreynaldii <reynaldi180101@gmail.com>
2024-03-27 09:57:58 +0700

    create responsive detail job

--- src/app/career/[slug]/page.jsx
+++ src/app/career/[slug]/page.jsx
@@ -17,38 +17,54 @@ const JobDetailsPage = ({ params: { slug } }) => {
       <Layout>
         {/* Banner Job Detail */}
         <section className="w-full bg-gradient-to-r from-secondary-500 to-primary-500">
-          <div className="container mx-auto py-20 ">
+          <div className="container mx-auto px-[22px] py-[58px] md:px-[40px] md:py-[44px] lg:py-20">
             <div className="flex flex-col gap-6">
               <div className="flex items-center justify-between">
-                <h1 className="py-8 text-center text-4xl font-bold text-white">
+                <h1 className="text-center text-[30px] font-bold text-white md:text-[40px]">
                   {job.name}
                 </h1>
                 <Link href={`/career/${job.slug}/submit-form`}>
-                  <button className="rounded-full bg-white px-6 py-3">
+                  <button className="hidden rounded-full bg-white px-6 py-3 md:block">
                     {' '}
                     Apply Now
                   </button>
                 </Link>
               </div>
-              <div className="flex flex-wrap items-start justify-between text-xl font-semibold text-white">
+              <div className="flex flex-wrap items-start justify-between font-semibold text-white">
                 <div className="flex flex-col gap-4">
                   <div className="flex items-center gap-7">
-                    <p className="min-w-[120px]">Division</p>
-                    <p>: {job.division}</p>
+                    <p className="min-w-[140px] max-w-[140px] text-[14px] font-semibold md:min-w-[245px] md:max-w-full md:text-[20px]">
+                      Department
+                    </p>
+                    <p className="text-[12px] md:text-[16px]">
+                      : {job.division}
+                    </p>
                   </div>
                   <div className="flex items-center gap-7">
-                    <p className="min-w-[120px]">Location</p>
-                    <p>: {job.location}</p>
+                    <p className="min-w-[140px] max-w-[140px] text-[14px] font-semibold md:min-w-[245px] md:max-w-full md:text-[20px]">
+                      Location
+                    </p>
+                    <p className="text-[12px] md:text-[16px]">
+                      : {job.location}
+                    </p>
                   </div>
                   <div className="flex items-center gap-7">
-                    <p className="min-w-[120px]">Availability</p>
-                    <p>: {job.availability}</p>
+                    <p className="min-w-[140px] max-w-[140px] text-[14px] font-semibold md:min-w-[245px] md:max-w-full md:text-[20px]">
+                      Position
+                    </p>
+                    <p className="text-[12px] md:text-[16px]">
+                      : {job.availability}
+                    </p>
                   </div>
                 </div>
-                <div className="flex flex-col gap-4">
-                  <div className="flex items-center gap-7">
-                    <p className="min-w-[120px]">Application is Open Until</p>
-                    <p>: 31 Maret 2024</p>
+                <div className="mt-4 flex flex-col gap-4 lg:mt-0">
+                  <div className="flex items-start gap-7 md:items-center">
+                    <p className="min-w-[140px] max-w-[140px] text-[14px] font-semibold md:min-w-[245px] md:max-w-full md:text-[20px]">
+                      Application is Open Until
+                    </p>
+                    <p className="text-[12px] md:text-[16px]">
+                      : 31 Maret 2024
+                    </p>
                   </div>
                 </div>
               </div>
@@ -57,19 +73,24 @@ const JobDetailsPage = ({ params: { slug } }) => {
         </section>
 
         {/* Description Section */}
-        <section className="w-full px-44 py-20">
-          <div className="container mx-auto flex flex-col items-start justify-center gap-16">
+        <section className="w-full px-[16px] py-[24px] md:px-10 md:py-10 lg:px-44 lg:py-20">
+          <div className="container mx-auto flex flex-col items-start justify-center gap-6 lg:gap-16">
             {/* overview */}
             <div className="flex flex-col gap-5">
-              <h2 className=" text-xl">Overview</h2>
-              <p>{job.overview}</p>
+              <h2 className="text-base md:text-xl">Overview</h2>
+              <p className="text-[12px] leading-[20px] md:text-[14px] md:leading-[24px]">
+                {job.overview}
+              </p>
             </div>
             {/* Key Responsibilites */}
             <div className="flex flex-col gap-5">
-              <h2 className=" text-xl">Key Responsibilites</h2>
+              <h2 className="text-base md:text-xl">Key Responsibilites</h2>
               <ul className="flex flex-col gap-2">
                 {job.key_responsibilities.map((key, index) => (
-                  <li className="ml-5 list-disc" key={index}>
+                  <li
+                    className="ml-5 list-disc text-[12px] leading-[20px] md:text-[14px] md:leading-[24px]"
+                    key={index}
+                  >
                     {key}
                   </li>
                 ))}
@@ -78,10 +99,13 @@ const JobDetailsPage = ({ params: { slug } }) => {
 
             {/* Key Qualifications */}
             <div className="flex flex-col gap-5">
-              <h2 className=" text-xl">Key Qualifications</h2>
+              <h2 className="text-base md:text-xl">Key Qualifications</h2>
               <ul className="flex flex-col gap-2">
                 {job.key_qualifications.map((key, index) => (
-                  <li className="ml-5 list-disc" key={index}>
+                  <li
+                    className="ml-5 list-disc text-[12px] leading-[20px] md:text-[14px] md:leading-[24px]"
+                    key={index}
+                  >
                     {key}
                   </li>
                 ))}
@@ -90,15 +114,19 @@ const JobDetailsPage = ({ params: { slug } }) => {
 
             {/* function of department */}
             <div className="flex flex-col gap-5">
-              <h2 className=" text-xl">Function of the Department</h2>
-              <p>{job.function_of_department}</p>
+              <h2 className="text-base md:text-xl">
+                Function of the Department
+              </h2>
+              <p className="text-[12px] leading-[20px] md:text-[14px] md:leading-[24px]">
+                {job.function_of_department}
+              </p>
             </div>
 
             {/* About Company */}
             <div className="flex max-w-[916px] flex-col gap-6 rounded-lg border border-solid border-gray-300 p-6 text-sm">
               <div className="flex flex-col gap-3">
-                <h2 className=" text-xl">About company</h2>
-                <p>
+                <h2 className="text-base md:text-xl">About company</h2>
+                <p className="text-[12px] leading-[20px] md:text-[14px] md:leading-[24px]">
                   Equinox Technology is a dynamic retail & SaaS enterprise,
                   spearheading innovation in the e-commerce retail space.
                   Specializing in a diverse range of products including home &
@@ -108,12 +136,12 @@ const JobDetailsPage = ({ params: { slug } }) => {
                 </p>
               </div>
               <div className="flex flex-col gap-3">
-                <h2 className=" text-xl">Office Address</h2>
-                <p>
+                <h2 className="text-base md:text-xl">Office Address</h2>
+                <p className="text-[12px] leading-[20px] md:text-[14px] md:leading-[24px]">
                   Pantai Indah Kapuk: Melody 2 Street No. 6, Pejaringan, North
                   Jakarta, Jakarta
                 </p>
-                <p>
+                <p className="text-[12px] leading-[20px] md:text-[14px] md:leading-[24px]">
                   Sahid Sudirman Center: 56th Floor, Unit C. Jend Sudirman
                   Street, Kav 86, Karet Tengsin, Tanah Abang, Central Jakarta,
                   Jakarta
@@ -133,7 +161,7 @@ const JobDetailsPage = ({ params: { slug } }) => {
         </section>
 
         {/* Related Job*/}
-        <section className=" px-44 py-20">
+        <section className="px-[16px] py-[24px] md:px-10 md:py-10 lg:px-44 lg:py-20">
           <div className="container mx-auto">
             <h2 className="mb-4 text-2xl font-semibold">Related Jobs</h2>
             <JobList jobs={relatedJobs} />
