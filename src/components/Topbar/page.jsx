ae580d5dfedb613298eb3e8b2ac31222131abd4c
muhreynaldii <reynaldi180101@gmail.com>
2024-03-27 13:20:54 +0700

    fix size topbar

--- src/components/Topbar/page.jsx
+++ src/components/Topbar/page.jsx
@@ -9,23 +9,23 @@ import { FaLinkedin } from 'react-icons/fa6';
 
 const Topbar = () => {
   return (
-    <nav className="z-[99] h-full max-h-[24px] bg-secondary-500 py-[6px] md:max-h-[32px] md:py-[8px]">
+    <nav className="z-[99] h-[24px] bg-secondary-500 py-[4px] md:h-[32px] md:pb-[8px]">
       <div className="container mx-auto px-4 sm:px-6 lg:px-0">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-[6px]">
-            <HiLanguage className="text-[12px] text-white md:text-[16px]" />
-            <p className="text-[10px] text-white md:text-[12px]">English</p>
+            <HiLanguage className="text-[12px] text-white md:text-[18px]" />
+            <p className="text-[12px] text-white md:text-[14px]">English</p>
             {/* <IoMdArrowDropdown className="text-[12px] text-white md:text-[16px]" /> */}
           </div>
           <div className="flex items-center gap-[12px] md:gap-[16px]">
             <Link href="tel:+628113000800">
-              <BiPhone className="text-[12px] text-white md:text-[16px]" />
+              <BiPhone className="text-[14px] text-white md:text-[18px]" />
             </Link>
             <Link href="/contact">
-              <MdMailOutline className="text-[12px] text-white md:text-[16px]" />
+              <MdMailOutline className="text-[14px] text-white md:text-[18px]" />
             </Link>
             <Link href="https://www.linkedin.com/company/equinoxtechnology">
-              <FaLinkedin className="text-[12px] text-white md:text-[16px]" />
+              <FaLinkedin className="text-[14px] text-white md:text-[18px]" />
             </Link>
           </div>
         </div>
